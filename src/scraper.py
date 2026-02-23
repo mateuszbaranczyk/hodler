7a7618aa78bf3d71cf0bc4e8197f018d415490b9
kundziorro <grzybek2413@gmail.com>
2026-02-23 18:03:13 +0100

    error handling in scraper

--- src/scraper.py
+++ src/scraper.py
@@ -1,8 +1,12 @@
 import httpx
 
 
+class ScraperError(Exception):
+    pass
+
+
 def get_html(url: str):
     response = httpx.get(url)
     if response.is_success:
         return response.content
-    return None
+    raise ScraperError
